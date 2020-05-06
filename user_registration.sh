a114140f0fdeaee81fb7d495b9f142647aaa6769
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-06 21:42:09 +0530

    added user_registration.sh

--- user_registration.sh
+++ user_registration.sh
@@ -1,4 +1,4 @@
-#! /bin/bash 
+#! /bin/bash -x
 read -p "enter first name  : " first_name
 
 if [[ $first_name =~ ^([[:upper:]][[:lower:]]+)+$ ]]
@@ -16,4 +16,11 @@ then
 else
    echo "please enter valid last name"
    echo "last letter capital and contains minimum three letters"
+fi
+read -p "enter a valid emial" email
+if [[ $email =~ ^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*\@[a-z]{1,}\.[a-z]{1,4}([.][a-z]{1,})*$ ]]
+then
+   echo "valid email"
+else
+   echo "invalid email" 
 fi
